2223e73ae63e9fa268a6ae70b10fa4807418524c
wgall <106452389+wgall@users.noreply.github.com>
2023-02-09 16:12:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
+/* Test */
 export default function Home() {
   return (
     <>
